9474756a24b85796cdc7e60466323de06ce85675
Le Duc Phong <le.duc.phong@gmail.com>
2020-12-05 20:33:00 -0400

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -5,8 +5,8 @@ def gcd(a, b):
         a, b = b, a % b
     return a
 
-def multiple_gcd(A):      # A is an array of intergers    
-    return reduce(gcd, b)   
+def multiple_gcd(arr):      # A is an array of intergers    
+    return reduce(gcd, arr)   
 
 if __name__ == '__main__':    
     print(multiple_gcd([16, 32, 40]))
